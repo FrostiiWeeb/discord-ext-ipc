14a87a1755bf8f7d714da94ee46710103b10f4c5
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-01-23 13:24:43 +0100

    yes

--- discord/ext/ipc/client.py
+++ discord/ext/ipc/client.py
@@ -75,7 +75,6 @@ class Client:
                 raise NotConnected("Server connection failed.")
 
             port_data = recv.json()
-            print(port_data)
             try:
                 self.port = port_data["port"]
             except KeyError:
